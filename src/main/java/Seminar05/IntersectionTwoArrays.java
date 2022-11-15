0a5ed22e30601d15886db3e925cbce2c65bc40b6
StanleyGunz <stanislavkinsler@gmail.com>
2022-11-15 15:26:46 +0700

    Seminar-05, HW Java GB

--- src/main/java/Seminar05/IntersectionTwoArrays.java
+++ src/main/java/Seminar05/IntersectionTwoArrays.java
@@ -16,14 +16,14 @@ public class IntersectionTwoArrays {
     public static int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        List<Integer> res = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         for(int i = 0, j = 0; i < nums1.length && j < nums2.length; ++i, ++j) {
-            if (nums1[i] == nums2[j]) res.add(nums1[i]);
+            if (nums1[i] == nums2[j]) result.add(nums1[i]);
             else if (nums1[i] > nums2[j]) i--;
             else j--;
         }
-        int[] r = new int[res.size()];
-        for(int i = 0; i < res.size(); ++i) r[i] = res.get(i);
+        int[] r = new int[result.size()];
+        for(int i = 0; i < result.size(); ++i) r[i] = result.get(i);
         return r;
     }
 }
